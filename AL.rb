f728bd749e2a87eb85b3846e91f9b4750d771eee
JustJace <jacebrewster@gmail.com>
2011-04-07 15:59:02 -0500

    Cleaned up solution, switched to hash, and finally (slowly) solved AL for large data set

--- AL.rb
+++ AL.rb
@@ -1,11 +1,19 @@
 #; Alien Language
 
-$words = ""
+# Hash language: -1 = Not There
+# => 			  0 = Partial
+# => 			  1 = Word
+$words = Hash.new(-1)
 
+# Gets the next token whether it's a single char
+# or a group of enclosed chars "(abc)"
+# removes the () and returns "abc"
 def next_token(w)
 	if w[0] != '('
 		return w.slice!(0)
 	end
+
+	w.slice!(0) #remove that leading '('
 
 	token = ""
 	while true
@@ -14,24 +22,28 @@ def next_token(w)
 			break
 		end
 
-		token += c
+		token << c
 	end
 
 	return token
 end
 
 
+# Recursive solve method.
+# Immediately trims if partial is not in hash
+# Returns with a +1 if the hash matches to a full word
+# Otherwise it gets the next token and continues the
+# brute force solve
 def solve(w, g, v)
 
-	if !$words.include?('|' + g)
+	if $words[g] == -1
 		return v
-	elsif $words.include?('|' + g + '|')
+	elsif $words[g] == 1
 		return v + 1
 	end
+	#else $words[g] == 0
 
-	tkn = next_token(w)
-	return v if tkn == "" || tkn == nil
-	
+	tkn = next_token(w)	
 	for char in tkn.chars
 		v = solve(String.new(w), g + char, v)
 	end
@@ -40,7 +52,7 @@ def solve(w, g, v)
 end
 
 
-#; __MAIN__
+#; MAIN
 
 
 LDN = gets.chomp.split(' ')
@@ -48,13 +60,22 @@ L = LDN[0].to_i
 D = LDN[1].to_i
 N = LDN[2].to_i
 
-for i in 1..D
-	$words += '|' + gets.chomp
-end
-$words += '|'
+$words[""] = 0 # Empty string is a partial too
 
+# Adds each word and every substring of that word to hash
+for i in 1..D
+	word = gets.chomp
+	partial = ""
+	for char in word.chars
+		partial << char
+		$words[partial] = 0
+	end
+	$words[word] = 1
+end
+
+# For each case, solve!
 for i in 1..N
 	tCase = gets.chomp
 	ans = solve(tCase, "", 0)
-	puts "Case #" + i.to_s + ": " + ans.to_s
+	puts "Case #" << i.to_s << ": " << ans.to_s
 end
