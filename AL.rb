fa9dd934436838357eaf01d76e9c41dd0165c714
JustJace <jacebrewster@gmail.com>
2011-04-07 10:13:01 -0500

    Added AL.rb to project repository

---
+++ AL.rb
@@ -0,0 +1,60 @@
+#; Alien Language
+
+$words = ""
+
+def next_token(w)
+	if w[0] != '('
+		return w.slice!(0)
+	end
+
+	token = ""
+	while true
+		c = w.slice!(0)
+		if c == ')'
+			break
+		end
+
+		token += c
+	end
+
+	return token
+end
+
+
+def solve(w, g, v)
+	
+	if !$words.include?('|' + g)
+		return v
+	elsif g.length == $L && $words.include?('|' + g + '|')
+		return v + 1
+	end
+
+	tkn = next_token(w)
+	return v if tkn == "" || tkn == nil
+	
+	for char in tkn.chars
+		v = solve(String.new(w), g + char, v)
+	end
+
+	return v
+end
+
+
+#; __MAIN__
+
+
+LDN = gets.chomp.split(' ')
+$L = LDN[0].to_i
+D = LDN[1].to_i
+N = LDN[2].to_i
+
+for i in 0...D
+	$words += '|' + gets.chomp
+end
+$words += '|'
+
+for i in 0...N
+	tCase = gets.chomp
+	ans = solve(tCase, "", 0)
+	puts "Case #" + (i + 1).to_s + ": " + ans.to_s
+end
